853bfd5368fc999aa55802c9eb6b1a5858a93e94
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2020-08-31 23:06:39 +0200

    Remove unused async keyword

--- api/dependencies/index.js
+++ api/dependencies/index.js
@@ -4,7 +4,7 @@ const packageJson = require("../../package.json");
 
 const VIEW_PATH = "views/dependencies.hbs";
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   const view = fs.readFileSync(VIEW_PATH).toString();
   const template = handlebars.compile(view);
   const dependencies = Object.entries(packageJson.dependencies).map(
